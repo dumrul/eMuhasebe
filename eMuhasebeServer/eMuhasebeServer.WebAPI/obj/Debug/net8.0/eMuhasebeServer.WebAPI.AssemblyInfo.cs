d42d1287f182ad086e7bc3e6a527b75f92b05d53
Mahmut Dumrul <dumrulmahmut@yahoo.com>
2025-03-28 16:23:42 +0300

    Create Client Project

--- eMuhasebeServer/eMuhasebeServer.WebAPI/obj/Debug/net8.0/eMuhasebeServer.WebAPI.AssemblyInfo.cs
+++ eMuhasebeServer/eMuhasebeServer.WebAPI/obj/Debug/net8.0/eMuhasebeServer.WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eMuhasebeServer.WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd8a61e80384f6dfc162c7cd46ffb88b5621e5a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f99538e41efc1fdae8bfb8b4b6f0f0092cb0171")]
 [assembly: System.Reflection.AssemblyProductAttribute("eMuhasebeServer.WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eMuhasebeServer.WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
